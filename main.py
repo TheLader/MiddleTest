005db3ef3c49f53cf610762e2ee7c125f4b04af9
TheLader <storozhuk.bogdan2015@gmail.com>
2024-04-05 11:24:05 +0300

    Change main code and add Pytest(Project finished)

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-def read_file(filename):
-    with open(filename, 'r') as file:
-        return set(file.read().splitlines())
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return set(file.readlines())
 
-def main():
-    file1 = 'file1.txt'
-    file2 = 'file2.txt'
-    same_lines = read_file(file1) & read_file(file2)
-    different_lines = read_file(file1) ^ read_file(file2)
+def write_to_file(file_path, lines):
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
 
-    with open('same.txt', 'w') as same_file:
-        same_file.write('\n'.join(same_lines))
+def compare_files(file1, file2, same_file, diff_file):
+    # Read contents of both files
+    file1_lines = read_file(file1)
+    file2_lines = read_file(file2)
 
-    with open('diff.txt', 'w') as diff_file:
-        diff_file.write('\n'.join(different_lines))
+    # Find common lines
+    common_lines = file1_lines.intersection(file2_lines)
+
+    # Write common lines to 'same.txt'
+    write_to_file(same_file, sorted(common_lines))
+
+    # Find different lines
+    different_lines = file1_lines.symmetric_difference(file2_lines)
+
+    # Write different lines to 'diff.txt'
+    write_to_file(diff_file, sorted(different_lines))
 
 if __name__ == "__main__":
-    main()
+    file1_path = 'file1.txt'
+    file2_path = "file2.txt"
+    same_file_path = "same.txt"
+    diff_file_path = "diff.txt"
+
+    compare_files(file1_path, file2_path, same_file_path, diff_file_path)
